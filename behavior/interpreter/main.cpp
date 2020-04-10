29e0b5afa6aeebd2111e19d7f17df5dce191c887
Filippo Cazzarolli <f.cazzarolli@ifinet.it>
2020-04-10 17:38:31 +0200

    Interpreter - fix

--- behavior/interpreter/main.cpp
+++ behavior/interpreter/main.cpp
@@ -11,9 +11,14 @@ namespace CppDesignPattern::Behavior::Interpreter {
         void simple() {
             Simple::RNInterpreter interpreter;
             vector<string> value = {"MCMXCVI", "MMMCMXCIX", "MMMM", "CXCX", "MDCLXVI", "DCCCLXXXVIII"};
-            auto it = value.begin();
-            while (++it != value.end()) {
-                cout << "Roman Numeral: " << *it << "   interpretation is " << interpreter.interpret(it->data())
+//            auto it = value.begin();
+//            while (++it != value.end()) {
+//                cout << "Roman Numeral: " << *it << "   interpretation is " << interpreter.interpret(it->data())
+//                     << endl;
+//            }
+
+            for (auto &&item : value) {
+                cout << "Roman Numeral: " << item << "   interpretation is " << interpreter.interpret(item.data())
                      << endl;
             }
         }
