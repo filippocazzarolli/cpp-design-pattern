8f01aa779d744700802e543930280303724d1489
Filippo Cazzarolli <f.cazzarolli@ifinet.it>
2020-04-10 09:11:45 +0200

    Interpreter - fix

--- behavior/interpreter/main.cpp
+++ behavior/interpreter/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include "simple.cpp"
 
 using namespace std;
@@ -8,21 +10,12 @@ namespace CppDesignPattern::Behavior::Interpreter {
     namespace {
         void simple() {
             Simple::RNInterpreter interpreter;
-            char input[20];
-            strcpy(input, "MCMXCVI");
-            cout << "Roman Numeral: " << input << "   interpretation is " << interpreter.interpret(input) << endl;
-            strcpy(input, "MMMCMXCIX");
-            cout << "Roman Numeral: " << input << "   interpretation is " << interpreter.interpret(input) << endl;
-            strcpy(input, "MMMM");
-            cout << "Roman Numeral: " << input << "   interpretation is " << interpreter.interpret(input) << endl;
-            strcpy(input, "MDCLXVIIII");
-            cout << "Roman Numeral: " << input << "   interpretation is " << interpreter.interpret(input) << endl;
-            strcpy(input, "CXCX");
-            cout << "Roman Numeral: " << input << "   interpretation is " << interpreter.interpret(input) << endl;
-            strcpy(input, "MDCLXVI");
-            cout << "Roman Numeral: " << input << "   interpretation is " << interpreter.interpret(input) << endl;
-            strcpy(input, "DCCCLXXXVIII");
-            cout << "Roman Numeral: " << input << "   interpretation is " << interpreter.interpret(input) << endl;
+            vector<string> value = {"MCMXCVI", "MMMCMXCIX", "MMMM", "CXCX", "MDCLXVI", "DCCCLXXXVIII"};
+            auto it = value.begin();
+            while (++it != value.end()) {
+                cout << "Roman Numeral: " << *it << "   interpretation is " << interpreter.interpret(it->data())
+                     << endl;
+            }
         }
     }
 
